af1e54a923326bf57e2de6bb0e989e7c8e0fa83a
pujichun <pujichun@outlook.com>
2020-08-31 04:16:23 +0800

    add jupyter notebook webUI style

---
+++ zsh/env.zsh
@@ -0,0 +1,7 @@
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$GOPATH:$GOBIN:$GOROOT/bin:$PATH
+export WORKON_HOME=$HOME/.PYTHON_ENVS
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python36
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/python36/bin/virtualenv
