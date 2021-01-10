a731850412e918fd3374b93a050335948511a9ba
pujichun <pujichun@outlook.com>
2021-01-10 15:16:19 +0800

    将插件管理器更改为zinit

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,21 +1,11 @@
-if [[ ! -d "~/.zplug" ]]; then
-	git clone https://github.com/zplug/zplug ~/.zplug
-fi
-source ~/.zplug/init.zsh
-
-# 语法高亮插件
-zplug "zsh-users/zsh-syntax-highlighting"
-
-# 语法补全插件
-zplug "zsh-users/zsh-autosuggestions"
-
-if ! zplug check --verbose; then
-	printf "Install?[y/N]: "
-	if read -q; then
-		echo; zplug install
-	else
-		echo
-	fi
-fi
-
-zplug load
+# zinit plugins setting
+zinit ice depth=1
+zinit light zsh-users/zsh-autosuggestions
+zinit ice depth=1
+zinit light zdharma/fast-syntax-highlighting
+zinit ice depth=1
+zinit light zsh-users/zsh-completions
+zinit ice depth=1
+zinit light zsh-users/zsh-history-substring-search
+zinit ice depth=1
+zinit light romkatv/powerlevel10k
