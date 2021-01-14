a30c72ae06f03b5be6fba55ea49be18c6c1fd82e
pujichun <pujichun@outlook.com>
2021-01-14 09:40:59 +0800

    删除ln.zsh

--- zsh/ln.zsh
+++
@@ -1,9 +0,0 @@
-ln -s -f ~/.config/tmux/tmxu.conf ~/.tmux.conf
-
-ln -s -f ~/.config/tmux/tmxu.conf.local ~/.tmux.conf.local
-
-ln -s /usr/bin/ranger /usr/bin/ra
-
-ln -s /usr/bin/neofetch /usr/bin/s
-
-ln -s -f ~/.config/zsh/.zsh_history ~/.zsh_history
