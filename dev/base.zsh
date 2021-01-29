8f64d19151321a31340a1033a72cef1decf005f5
pujichun <pujichun@outlook.com>
2021-01-29 22:43:58 +0800

    modify bash.zsh

--- dev/base.zsh
+++ dev/base.zsh
@@ -19,9 +19,5 @@ pip3 install neovim
 mkdir ~/.zinit
 git clone --depth=1 https://github.com/zdharma/zinit.git ~/.zinit/bin
 ln -s -f ~/.config/zsh/zshrc .zshrc
-ln -s -f ~/.config/tmux/tmxu.conf ~/.tmux.conf
-ln -s -f ~/.config/tmux/tmxu.conf.local ~/.tmux.conf.local
-ln -s /usr/bin/ranger /usr/bin/ra
-ln -s /usr/bin/neofetch /usr/bin/s
 ln -s -f ~/.config/zsh/.zsh_history ~/.zsh_history
 source ~/.zshrc
