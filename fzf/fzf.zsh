cfe6d3021b5a86ba9485764a8f313ecb6a8bb12c
pujichun <pujichun@outlook.com>
2021-01-31 23:25:31 +0800

    add fzf

---
+++ fzf/fzf.zsh
@@ -0,0 +1,3 @@
+export FZF_DEFAULT_OPTS='--preview "[[ $(file --mime {}) =~ binary ]] && echo {} is binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_COMPLETION_TRIGGER='\'
