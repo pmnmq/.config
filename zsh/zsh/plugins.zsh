dc6a88d0ed113d28088bd1e869dde60cc1bd6576
pujichun <pujichun@outlook.com>
2021-02-11 11:44:02 +0800

    modify zsh

---
+++ zsh/zsh/plugins.zsh
@@ -0,0 +1,11 @@
+# zinit plugins setting
+zinit ice depth=1
+zinit light zsh-users/zsh-autosuggestions
+zinit ice depth=1
+zinit light zdharma/fast-syntax-highlighting
+zinit ice depth=1
+zinit light zsh-users/zsh-completions
+zinit ice depth=1
+zinit light zsh-users/zsh-history-substring-search
+zinit ice depth=1
+zinit light romkatv/powerlevel10k
