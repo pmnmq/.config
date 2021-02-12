66ef9f79578e5e8689bfc0f07601302af1868142
pujichun <pujichun@outlook.com>
2021-02-12 11:18:53 +0800

    setting environment variable

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,15 +1,15 @@
 # Go
-export GOROOT=/usr/local/go
+export GOROOT=/usr/lib/go
 export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
 export PATH=$GOPATH:$GOBIN:$GOROOT/bin:$PATH
 export GO111MODULE=on
-export GOPROXY=https://goproxy.io
+export GOPROXY=https://goproxy.cn
 
 # Python VirtualEnvWrapper
-export WORKON_HOME=$HOME/.PYTHON_ENVS
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python36
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/python36/bin/virtualenv
+# export WORKON_HOME=$HOME/.PYTHON_ENVS
+# export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python36
+# export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/python36/bin/virtualenv
 
 # hadoop
 export HADOOP_HOME=/usr/local/hadoop
